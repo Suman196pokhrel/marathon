a7407aa62ea6f5e2253170f0320858435d131e2c
Suman Pokhrel <spokhrel196@gmail.com>
2026-09-14 04:55:22 +0930

    Add longest common prefix solution

---
+++ block-4-easy-questions-leetcode/arrays/longest_prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ""
+
+        for i in range(len(strs[0])):
+            for word in strs:
+                if len(word) == i or word[i] != strs[0][i]:
+                    return result
+
+            result += strs[0][i]
+        return result
+        
+
